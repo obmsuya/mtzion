df8cc6edf21bf77144ebc101bae46188793cdb02
obmsuya <obmsuya@yahoo.com>
2018-02-28 12:13:50 +0300

    My mount zion webapp

---
+++ accounts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    Proffession = models.CharField (max_length=100, default='')
+    country = models.CharField (max_length=100, default='')
+    region = models.CharField (max_length=100, default='')
+    city = models.CharField (max_length=100, default='')
+    website = models.URLField(default='')
+    phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_image',null=True, blank=True)
+
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+            user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
